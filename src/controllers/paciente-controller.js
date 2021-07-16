1a53a59f0d7ecc74b0658a923b7baaa3a98b21d8
Daniel-Roch <daniel_roch4@hotmail.com>
2021-07-16 20:29:49 -0300

    Criando os arquivos paciente-controller e PacienteModels para já começar a implementar seus devidos códigos, e já no arquivos server.js já implementei uma rota para o paciente-controller

---
+++ src/controllers/paciente-controller.js
@@ -0,0 +1,4 @@
+//Exportando a rota - e recebendo o parametro app.
+module.exports = app =>{
+
+}
--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const app = express()
 
-//chamando o express.json() para ler aquivos que o req enviar.
+//chamando o express.json() para ler aquivos que o req, e multi envios.
 app.use(express.json())
-//Ler - multi envios.
 app.use(express.urlencoded({extended: true}))
 
-
+//Chamando a rota do controller - e passando o express para ele.
+const rotaPaciente = require('./controllers/paciente-controller')
+rotaPaciente(app)
 
 //Abrindo a porta:
 app.listen(3050,()=>{console.log('Port: http://localhost:3050')})
