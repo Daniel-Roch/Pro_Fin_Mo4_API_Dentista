e773e1190e6835531033c5f19721f0ccc1d510b5
Daniel-Roch <daniel_roch4@hotmail.com>
2021-07-24 00:02:42 -0300

    Adicionando mais um teste - Testando o Patch, envio de dados de trocando nome

--- src/_test_/api.test.js
+++ src/_test_/api.test.js
@@ -24,11 +24,20 @@ describe('Testando rotas de Paciente',()=>{
             expect(res.statusCode).toEqual(201)
             expect(res.body).toMatchObject({'Envio de dados': true})
     })
-    //Rosta DELETE
+    //Rota PATCH - Alterar parcialmente
+    it('Fazendo um PATCH - alteração parcial!', async ()=>{
+        const res = await request(app)
+        .patch('/paciente/111.111.111-01')
+        .send({
+            nome: "Ciclano"
+        })
+        expect(res.statusCode).toEqual(200)
+        expect(res.body).toMatchObject({ 'Dado alterado': 'nome' })
+    })
+    //Rota DELETE
     it('Delete /paciente/cpf certo',async ()=>{
         const res = await request(app)
         .delete('/paciente/111.111.111-01')
-        console.log(res.body)
         expect(res.statusCode).toEqual(200)
         expect(res.body).toMatchObject({ Delete: true })
     })
