318e6b235c7878b6c127eb8dfb1f30782025903f
Daniel de Lemos Rocha <81339913+Daniel-Roch@users.noreply.github.com>
2021-07-28 17:56:56 -0300

    Colocando o link do heroku

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const app = require('./app')
 
+//-> Link Heroku: https://projeto-dentista-api-m4.herokuapp.com/paciente
+
 //Abrindo servidor.
 app.listen(process.env.PORT || 3050,()=>{console.log('Port: http://localhost:3050')})
